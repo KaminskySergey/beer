77612ed65b666cb36cdd43b6a5b6425fde6564b9
KaminskySergey <kaminsky_s_s@ukr.net>
2023-07-18 00:28:31 +0300

    g

--- src/components/screen/home/Home.tsx
+++ src/components/screen/home/Home.tsx
@@ -6,24 +6,20 @@ import { Beer } from "@/types/beer.types"
 import Link from "next/link"
 import { FC, PropsWithChildren, useEffect, useState } from "react"
 
-const styles = {
-    container: {
-      margin: '0 auto',
-      marginTop: '128px',
-      width: '300px',
-      textAlign: 'center',
-      background: 'linear-gradient(#000eff, #aecb19)',
-      borderRadius: '10px',
+
+
+const HomePage: FC = () => {
+
+return (
+<>
+
+<div style={{margin: '0 auto', marginTop: '128px', width: '300px', background: 'linear-gradient(#000eff, #aecb19)', borderRadius: '10px',
       padding: '20px',
-      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-    },
-    heading: {
-      fontSize: '24px',
+      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',}}>
+<h1  style={{fontSize: '24px',
       color: '#333',
-      marginBottom: '20px',
-    },
-    link: {
-      display: 'inline-block',
+      marginBottom: '20px'}}>Beer</h1>
+<Link href={'/beer'}  style={{display: 'inline-block',
       backgroundColor: '#4caf50',
       color: '#fff',
       padding: '10px 20px',
@@ -31,19 +27,7 @@ const styles = {
       textDecoration: 'none',
       fontSize: '16px',
       fontWeight: 'bold',
-      transition: 'background-color 0.3s ease',
-    },
-    
-  };
-
-const HomePage: FC = () => {
-
-return (
-<>
-
-<div style={styles.container}>
-<h1 style={styles.heading}>Beer</h1>
-<Link href={'/beer'} style={styles.link}>
+      transition: 'background-color 0.3s ease',}}>
     Get Start
 </Link>
 </div>
